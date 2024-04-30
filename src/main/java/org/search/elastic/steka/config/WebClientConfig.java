a29b26868cab61e3fbba6e6727b7c65b89a2f188
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-04-30 22:29:38 +0900

    feat: 쿼리스트링 추가 필터 구현 ( 시간복잡도 =>  O(n) )

--- src/main/java/org/search/elastic/steka/config/WebClientConfig.java
+++ src/main/java/org/search/elastic/steka/config/WebClientConfig.java
@@ -3,10 +3,15 @@ package org.search.elastic.steka.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * packageName    : org.search.elastic.steka.openapi.config
@@ -29,16 +34,39 @@ public class WebClientConfig {
     public WebClient movieClient() {
         return WebClient.builder()
                 .baseUrl("http://www.kobis.or.kr/kobisopenapi/webservice/rest")
-                .filter(addServiceKey("key", serviceKey))
+                .filter(addQueryString("key", serviceKey))
                 .build();
     }
     
-    private ExchangeFilterFunction addServiceKey(String param, String value) {
-        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-            String originalUrl = clientRequest.url().toString();
-            String newUrl = originalUrl + (originalUrl.contains("?") ? "&" : "?") + param + "=" + value;
+    private ExchangeFilterFunction addQueryString(String param, String value) {
+        return (clientRequest, nextFilter) -> {
+            URI oldUri = clientRequest.url();
+            String originalQuery = oldUri.getQuery() != null ? oldUri.getQuery() : "";
             
-            return clientRequest.url(URI.create(newUrl)).build();
-        });
+            // 쿼리 스트링에서 param이 있는지 확인하고, 없으면 추가하거나 있으면 교체
+            String newQuery = Stream.of(originalQuery.split("&"))
+                    .map(s -> s.split("="))
+                    .collect(Collectors.toMap(s -> s[0], s -> s.length > 1 ? s[1] : ""))
+                    .entrySet()
+                    .stream()
+                    .map(entry -> Objects.equals(entry.getKey(), param) ? param + "=" + value : entry.getKey() + "=" + entry.getValue())
+                    .collect(Collectors.joining("&"));
+            
+            // param이 원래 쿼리에 없었다면 추가
+            if (!newQuery.contains(param + "=")) {
+                newQuery = newQuery.isEmpty() ? param + "=" + value : newQuery + "&" + param + "=" + value;
+            }
+            
+            URI newUri = UriComponentsBuilder.fromUri(oldUri)
+                    .replaceQuery(newQuery)
+                    .build(true)
+                    .toUri();
+            
+            ClientRequest filteredRequest = ClientRequest.from(clientRequest)
+                    .url(newUri)
+                    .build();
+            
+            return nextFilter.exchange(filteredRequest);
+        };
     }
 }
