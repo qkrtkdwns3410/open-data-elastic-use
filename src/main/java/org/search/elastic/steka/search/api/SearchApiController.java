1b6076dbf3c5dd0946e56f96765d74c25e651162
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-05-04 00:26:51 +0900

    chore: 컨트롤러 정의

---
+++ src/main/java/org/search/elastic/steka/search/api/SearchApiController.java
@@ -0,0 +1,23 @@
+package org.search.elastic.steka.search.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : org.search.elastic.steka.search.api
+ * fileName       : SearchApiController
+ * author         : ipeac
+ * date           : 24. 5. 4.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 5. 4.        ipeac       최초 생성
+ */
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class SearchApiController {
+
+}
